955e696db18e9b10e516e01bd3481f7365674817
ilia24 <iliademertchian24@gmail.com>
2017-05-04 15:08:50 -0400

    Add skeleton ajax request linked to tweet button

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$('#create-tweet').on('click', function(e) {
+  e.preventDefault();
+  $(function() {
+
+    $.ajax({
+      method: 'GET',
+      url: '',
+      dataType: 'html'
+    }).done(function(data){
+      console.log('ajax submission succeeded')
+
+      };
+    }).fail(function(){
+      console.log('ajax submission failed')
+
+    }).always(function(){
+    });
+
+  });
+});
